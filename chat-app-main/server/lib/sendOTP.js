822878757a82787f4826efe7b07a221c6694c24d
singhkunall <your-email@example.com>
2026-05-11 17:24:05 +0530

    switch to brevo smtp

--- chat-app-main/server/lib/sendOTP.js
+++ chat-app-main/server/lib/sendOTP.js
@@ -3,9 +3,9 @@ import nodemailer from "nodemailer";
 export const sendOTPEmail = async (email, otp) => {
   try {
     const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
